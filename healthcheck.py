ea0a76724ec5b2111e71ca7812a7f2c1f4dc5bbe
hakermania <alexsol.developer@gmail.com>
2016-08-02 11:33:52 +0300

    Code cleanup

--- healthcheck.py
+++ healthcheck.py
@@ -12,51 +12,52 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-if len(sys.argv) != 2:
-	print 'Parse the ini file'
-	sys.exit(1)
+def main():
+	if len(sys.argv) != 2:
+		print 'Parse the ini file'
+		sys.exit(1)
 
-config = configparser.ConfigParser()
-config.read(sys.argv[1])
+	config = configparser.ConfigParser()
+	config.read(sys.argv[1])
 
-sectionCounter = 0
-for section in config.sections():
-	sectionCounter += 1
+	sectionCounter = 0
+	for section in config.sections():
+		sectionCounter += 1
 
-	print
+		print
 
-	try:
-		site = config[section]['site']
-		check = config[section]['check']
-		print bcolors.HEADER + '--->', str(sectionCounter) + '.', section, '(' + site + ') ->', check + bcolors.ENDC
-	except:
-		print bcolors.FAIL + 'Malformed section', section + bcolors.ENDC
-		continue
+		try:
+			site = config[section]['site']
+			check = config[section]['check']
+			print bcolors.HEADER + '--->', str(sectionCounter) + '.', section, '(' + site + ') ->', check + bcolors.ENDC
+		except:
+			print bcolors.FAIL + 'Malformed section', section + bcolors.ENDC
+			continue
 
-	request = urllib2.Request(site)
+		request = urllib2.Request(site)
 
-	if 'username' in config[section] and 'password' in config[section]:
-		# authentication is needed
-		print bcolors.INFO + 'Using basic auth for', section + bcolors.ENDC
-		base64string = base64.encodestring('%s:%s' % (config[section]['username'], config[section]['password'])).replace('\n', '')
-		request.add_header('Authorization', 'Basic %s' % base64string)
+		if 'username' in config[section] and 'password' in config[section]:
+			# authentication is needed
+			print bcolors.INFO + 'Using basic auth for', section + bcolors.ENDC
+			base64string = base64.encodestring('%s:%s' % (config[section]['username'], config[section]['password'])).replace('\n', '')
+			request.add_header('Authorization', 'Basic %s' % base64string)
 
-	try:
-		req = urllib2.urlopen(request)
-	except urllib2.HTTPError as e:
-		print bcolors.FAIL + 'Could not get', site, '(' + str(e) + ')' + bcolors.ENDC
-		continue
+		try:
+			req = urllib2.urlopen(request)
+		except urllib2.HTTPError as e:
+			print bcolors.FAIL + 'Could not get', site, '(' + str(e) + ')' + bcolors.ENDC
+			continue
 
-	content = req.read()
+		content = req.read()
 
-	encoding = req.headers['content-type'].split('charset=')[-1]
+		encoding = req.headers['content-type'].split('charset=')[-1]
 
-	if encoding == 'text/html':
-		ucontent = unicode(content, chardet.detect(content)['encoding'])
-	else:
-		ucontent = unicode(content, encoding)
+		if encoding == 'text/html':
+			ucontent = unicode(content, chardet.detect(content)['encoding'])
+		else:
+			ucontent = unicode(content, encoding)
 
-	print bcolors.OK + 'OK' + bcolors.ENDC if check in ucontent else bcolors.FAIL + 'FAIL' + bcolors.ENDC
+		print bcolors.OK + 'OK' + bcolors.ENDC if check in ucontent else bcolors.FAIL + 'FAIL' + bcolors.ENDC
 
-# print config
-# print config.sections()
+if __name__ == '__main__':
+	main()
